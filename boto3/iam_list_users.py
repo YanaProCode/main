59969b67d4a9ab9e941f77173b88a07439931ebf
Yana Proshak <yana_proshak@epam.com>
2025-01-18 19:42:31 +0100

    Add boto3 script

---
+++ boto3/iam_list_users.py
@@ -0,0 +1,7 @@
+import boto3
+from pprint import pprint
+aws_management_console = boto3.session.Session(profile_name="default")
+iam_console = aws_management_console.client(service_name="iam")
+result = iam_console.list_users()
+for each_user in result['Users']:
+    print(each_user['UserName'])
